98fce6313eaabdf47a6c1db0889eda97071bafbb
Lukas <lukass135@gmail.com>
2017-11-10 16:35:46 +0100

    header pre generovanie kodu

---
+++ Source/GenCode.h
@@ -0,0 +1,14 @@
+//
+// Created by Lukas on 10.11.2017.
+//
+
+#ifndef FREEBASIC_COMPILER_GENCODE_H
+#define FREEBASIC_COMPILER_GENCODE_H
+
+
+void generateCode();
+
+
+
+
+#endif //FREEBASIC_COMPILER_GENCODE_H
