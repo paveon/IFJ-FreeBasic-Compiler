991b855c95c7a135d6e0ec3e11d758293b1e217d
Lukas <lukass135@gmail.com>
2017-11-11 17:42:05 +0100

    praca s generovanim outputu

--- Source/CodeGenerator.c
+++ Source/CodeGenerator.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "CodeGenerator.h"
 #include "CompilationErrors.h"
+#include "LLtable.h"
+#include <string.h>
 
 #define RULE_CHUNK 100
 #define CODE_CHUNK 1024
@@ -28,6 +30,66 @@ static TokenArray g_Tokens;
 
 
 void GenerateCode(void) {
+	int rule;
+	int tokenPos = 0;
+	char tmp[CODE_CHUNK];
+	TokenType type;
+	const void* value;
+
+	if (g_Rules.used > 0) {
+		printf("BLOCK START\n");
+
+		for (size_t j = 0; j < g_Rules.used; j++) {
+			memset(tmp, 0, CODE_CHUNK);
+			rule = (int)g_Rules.buffer[j];
+			printf ("pravidla: %d\n", rule);
+
+			switch (rule) {
+				case RULE_EPSILON: // 1
+				case RULE_MAIN_SCOPE: // 2
+				case RULE_ST_LIST: // 9
+					break;
+				case RULE_ST_VAR_DECL: // 10
+					while (GetTokenType(g_Tokens.array[tokenPos]) != TOKEN_IDENTIFIER) tokenPos++; // hladame identifikator
+					value = GetTokenValue(g_Tokens.array[tokenPos]);
+					tokenPos++; // preskocime identifikator
+					switch((int)g_Rules.buffer[j+1]) {
+						case RULE_TYPE_INT:
+							sprintf(tmp, "DEFVAR LF@%s\nMOVE LF@%s 0\n", (char*)value, (char*)value); // TODO LF GF - inicializacia na 0
+							break;
+						case RULE_TYPE_DOUBLE:
+							sprintf(tmp, "DEFVAR LF@%s\nMOVE LF@%s 0.0\n", (char*)value, (char*)value); // TODO LF GF - inicializacia na 0.0
+							break;
+						case RULE_TYPE_STRING:
+							sprintf(tmp, "DEFVAR LF@%s\nMOVE LF@%s !""\n", (char*)value, (char*)value); // TODO LF GF - inicializacia na prazdny string
+							break;
+					}
+
+					PushString(tmp);
+					printf ("%s strlen je: %d\n", g_Code.buffer, (int)strlen((char*)g_Code.buffer));
+					printf ("%d je size, %d je used\n", (int)g_Code.size, (int)g_Code.used);
+
+				default:
+					break;
+
+			}
+
+
+
+
+
+
+
+		}
+	}
+
+
+
+
+
+
+
+	/*
 	TokenType type;
 	const void* value;
 	if (g_Tokens.used > 0) {
@@ -62,7 +124,33 @@ void GenerateCode(void) {
 		printf("-- BLOCK END\n\n");
 		g_Tokens.used = 0;
 	}
+	 */
 }
+
+
+void PushString(char *newString) {
+	size_t length = strlen(newString);
+
+	if (g_Code.size - g_Code.used <= length) {
+		uchar* tmp;
+		if ((tmp = realloc(g_Code.buffer, sizeof(uchar) * (g_Code.size + CODE_CHUNK))) == NULL) {
+			FatalError(ER_FATAL_INTERNAL);
+		}
+		g_Code.buffer = tmp;
+		if (g_Code.size == 0) { // prvy krat je potreba memsetnut, inak su tam nejake smeti
+			memset(g_Code.buffer, 0, CODE_CHUNK);
+		}
+		g_Code.size += CODE_CHUNK;
+	}
+
+
+	strcat((char*)g_Code.buffer, newString);
+
+	g_Code.used = g_Code.used + length;
+
+}
+
+
 
 void PushToken(Token* token) {
 	if (g_Tokens.used == g_Tokens.size) {
